b6100bda1bde712ef039c609e51610be65c1b4d9
Fanaen <contact@fanaen.fr>
2015-04-18 13:18:44 +0200

    Add the triskelrang

--- love2d/main.lua
+++ love2d/main.lua
@@ -1,16 +1,15 @@
 -- Load some default values for our rectangle.
 function love.load()
-    x, y, w, h = 20, 20, 60, 20;
+    x, y, w, h = 20, 20, 20, 20;
+    triImage = love.graphics.newImage("images/triskelrang.png");
 end
 
 -- Increase the size of the rectangle every frame.
 function love.update(dt)
-    w = w + 1;
-    h = h + 1;
+    
 end
 
 -- Draw a coloured rectangle.
 function love.draw()
-    love.graphics.setColor(0, 0, 100);
-    love.graphics.rectangle('fill', x, y, w, h);
+    love.graphics.draw(triImage, x, y);
 end
